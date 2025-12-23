89d56b93d10f169e8ced2a61beac1908e38e1fb2
Guilherme Setera <guilhermesetera@pm.me>
2025-12-23 11:52:30 -0300

    Fix: Corrige erro que impedia o updade

--- src/modules/vendas/repository/queries/postgres/update_pagamento.sql
+++ src/modules/vendas/repository/queries/postgres/update_pagamento.sql
@@ -1,16 +1,8 @@
 UPDATE elysium.vendas
 SET
-  valor_pago = COALESCE(valor_pago, 0) + :valor_pago,
-  pago = CASE
-           WHEN COALESCE(valor_pago, 0) + :valor_pago >= COALESCE(total, 0)
-           THEN TRUE
-           ELSE FALSE
-         END,
-  data_pagamento = CASE
-                     WHEN COALESCE(valor_pago, 0) + :valor_pago >= COALESCE(total, 0)
-                     THEN COALESCE(:data_pagamento, CURRENT_TIMESTAMP)
-                     ELSE data_pagamento
-                   END
+  valor_pago = COALESCE(total, valor_pago, 0),
+  pago = TRUE,
+  data_pagamento = COALESCE(data_pagamento, CURRENT_TIMESTAMP)
 WHERE id = :venda_id
   AND cancelada = FALSE
 RETURNING id, pago, valor_pago;
